d68fca9db50409df9b2de5fb512ad9b0e3bd020e
JunHyung1206 <pjh120610@naver.com>
2025-10-29 14:46:49 +0900

    test(DrawLottoNum) : 로또에 대한 테스트 코드 변경

--- src/test/java/lotto/domain/PaymentTest.java
+++ src/test/java/lotto/domain/PaymentTest.java
@@ -15,27 +15,20 @@ class PaymentTest {
     @Test
     @DisplayName("정상적인 입력에 대해서는 동작한다")
     void successCase() {
-        String input = "1000";
+        int input = 1000;
         payment = new Payment(input);
         assertThat(payment.getValue()).isEqualTo(1000);
-    }
-
-    @Test
-    @DisplayName("구매 금액은 정수의 형태여야 한다.")
-    void notIntegerCase() {
-        String input = "abcd";
-        assertThrows(IllegalArgumentException.class, () -> new Payment(input));
     }
 
 
     @ParameterizedTest
     @DisplayName("구매 금액은 양의 정수여야 한다.")
-    @ValueSource(strings = {
-            "-1000",
-            "-2000",
-            "0"
+    @ValueSource(ints = {
+            -1000,
+            -2000,
+            0
     })
-    void notPositiveCase(String input) {
+    void notPositiveCase(int input) {
         assertThrows(IllegalArgumentException.class, () -> new Payment(input));
     }
 }
