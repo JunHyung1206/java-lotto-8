339d67ee112a35318bdfe328d101e4c0c1756393
JunHyung1206 <pjh120610@naver.com>
2025-10-29 14:47:39 +0900

    test(DrawLottoNum) : 코드 변경에 따른 테스트 코드 삭제

--- src/main/java/lotto/domain/Payment.java
+++ src/main/java/lotto/domain/Payment.java
@@ -3,18 +3,12 @@ package lotto.domain;
 public class Payment {
     private final int value;
 
-    public Payment(String paymentValue) {
-        validate(paymentValue);
-        this.value = toInteger(paymentValue);
+    public Payment(int payment) {
+        validate(payment);
+        this.value = payment;
     }
 
-    private void validate(String paymentValue) {
-        int payment;
-        try{
-            payment = toInteger(paymentValue);
-        } catch(NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 숫자만 입력해주세요.");
-        }
+    private void validate(int payment) {
         if (payment <= 0) {
             throw new IllegalArgumentException("[ERROR] 구매액은 1원 이상입니다.");
         }
@@ -23,9 +17,6 @@ public class Payment {
         }
     }
 
-    private static int toInteger(String paymentValue) {
-        return Integer.parseInt(paymentValue);
-    }
 
     public int getValue() {
         return value;
