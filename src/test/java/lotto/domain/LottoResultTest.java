a94d297694870d98f406304df3b12e28d982719e
JunHyung1206 <pjh120610@naver.com>
2025-10-31 05:22:30 +0900

    test(GetResult) : 결과 확인에 대한 테스트 코드 작성

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -1,48 +1,30 @@
 package lotto.domain;
 
+import lotto.config.WinningRank;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
 
 class LottoResultTest {
+    LottoResult lottoResult;
+    WinningNumbers winningNumbers;
 
-    List<Lotto> lottos;
-    Lotto lotto;
-    BonusNumber bonusNumber;
     @BeforeEach
     void setUp() {
-        lottos = new ArrayList<>();
-        lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-        bonusNumber = new BonusNumber(7, lotto);
-
-        List<List<Integer>> sample = List.of(List.of(8, 21, 30, 32, 38, 41),
-                List.of(4, 7, 10, 17, 19, 41),
-                List.of(1, 2, 4, 23, 33, 36),
-                List.of(8, 21, 25, 26, 38, 42),
-                List.of(1, 2, 3, 4, 5, 7),
-                List.of(1, 2, 3, 4, 5, 8),
-                List.of(7, 31, 34, 36, 42, 43),
-                List.of(18, 20, 21, 26, 34, 38),
-                List.of(6, 15, 16, 24, 32, 35),
-                List.of(4, 6, 8, 17, 25, 45),
-                List.of(1, 2, 3, 4, 5, 6));
-
-        for (List<Integer> integers : sample) {
-            lottos.add(new Lotto(integers));
-        }
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = new BonusNumber(7, winningLotto);
+        winningNumbers = new WinningNumbers(winningLotto, bonusNumber);
     }
 
     @Test
-    void successTest(){
-        LottoResult lottoResult = new LottoResult(lottos, lotto, bonusNumber);
-        // Map<Integer, Integer> result = lottoResult.getResult();
-        // System.out.println("result = " + result);
+    void successTest() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        lottoResult = new LottoResult(lotto, winningNumbers);
+        WinningRank result = lottoResult.getResult();
+        Assertions.assertThat(result).isInstanceOf(WinningRank.class);
+        Assertions.assertThat(result).isEqualTo(WinningRank.FIRST);
     }
-
-
 }
