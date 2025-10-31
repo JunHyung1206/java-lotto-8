595243f58a627a8d1cac1b9ae8372b5f3400e96d
JunHyung1206 <pjh120610@naver.com>
2025-11-01 02:57:51 +0900

    test(SaleLotto) : 리팩토링으로 인한 테스트 코드 변경

--- src/test/java/lotto/service/LottoPurchaseServiceTest.java
+++ src/test/java/lotto/service/LottoPurchaseServiceTest.java
@@ -1,5 +1,7 @@
-package lotto.domain;
+package lotto.service;
 
+import lotto.domain.Lotto;
+import lotto.domain.Payment;
 import lotto.domain.lottogenerator.RandomLottoGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,26 +10,26 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class LottoSellerTest {
-    LottoSeller lottoSeller;
+class LottoPurchaseServiceTest {
+    LottoPurchaseService lottoPurchaseService;
 
     @Test
     @DisplayName("각 구매액수에 맞게 로또를 생성한다.")
     void successCase() {
         // given
-        lottoSeller = new LottoSeller(new RandomLottoGenerator());
+        lottoPurchaseService = new LottoPurchaseService(new RandomLottoGenerator());
         Payment payment1 = new Payment(8000);
         Payment payment2 = new Payment(100000);
 
         // when
-        List<Lotto> lottos1 = lottoSeller.sellLottos(payment1);
-        List<Lotto> lottos2 = lottoSeller.sellLottos(payment2);
+        List<Lotto> lottos1 = lottoPurchaseService.purchase(payment1);
+        List<Lotto> lottos2 = lottoPurchaseService.purchase(payment2);
 
         // then
         assertThat(lottos1).hasSize(8);
         assertThat(lottos2).hasSize(100);
         assertThat(lottos1.getFirst().getNumbers()).hasSize(6);
         assertThat(lottos2.getFirst().getNumbers()).hasSize(6);
-
     }
+
 }
