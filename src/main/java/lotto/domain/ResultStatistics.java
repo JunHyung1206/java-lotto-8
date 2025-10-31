77e98844b0a38ffbaa1f2770ccec056af010af7e
JunHyung1206 <pjh120610@naver.com>
2025-10-31 13:48:23 +0900

    refactor(GetTotalPrize) : 모든 로또의 결과를 계산하는 클래스 생성

---
+++ src/main/java/lotto/domain/ResultStatistics.java
@@ -0,0 +1,45 @@
+package lotto.domain;
+
+import lotto.config.WinningRank;
+
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+public class ResultStatistics {
+    private final List<Lotto> lottos;
+    private final WinningNumbers winningNumbers;
+    private Map<WinningRank, Integer> result;
+
+    public ResultStatistics(List<Lotto> lottos, WinningNumbers winningNumbers) {
+        this.lottos = lottos;
+        this.winningNumbers = winningNumbers;
+        calculateStatistics();
+    }
+
+
+    private void calculateStatistics() {
+        result = new EnumMap<>(WinningRank.class);
+        for (Lotto lotto : lottos) {
+            WinningRank rank = new LottoResult(lotto, winningNumbers).getResult();
+            if (rank == WinningRank.NONE) {
+                continue;
+            }
+            result.put(rank, result.getOrDefault(rank, 0) + 1);
+        }
+    }
+
+    public int calculatePrize() {
+        int prize = 0;
+        for (WinningRank winningRank : result.keySet()) {
+            prize += winningRank.getPrize() * result.get(winningRank);
+        }
+        return prize;
+    }
+
+
+    public Map<WinningRank, Integer> getResult() {
+        return result;
+    }
+
+}
