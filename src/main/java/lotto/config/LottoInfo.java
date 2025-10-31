f8ac8adb6368901970a4cf60122aaafd3dcba35c
JunHyung1206 <pjh120610@naver.com>
2025-10-31 19:22:18 +0900

    fix(all) : 상수처리 수정

--- src/main/java/lotto/config/LottoInfo.java
+++ src/main/java/lotto/config/LottoInfo.java
@@ -1,10 +1,10 @@
 package lotto.config;
 
 public final class LottoInfo {
-    public static int MIN_VALUE = 1;
-    public static int MAX_VALUE = 45;
-    public static int PICK_NUMBER_COUNT = 6;
-    public static int LOTTO_PRICE = 1000;
+    public static final int MIN_VALUE = 1;
+    public static final int MAX_VALUE = 45;
+    public static final int PICK_NUMBER_COUNT = 6;
+    public static final int LOTTO_PRICE = 1000;
 
     private LottoInfo(){
     }
