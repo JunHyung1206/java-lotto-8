31a7d2c89e19a4bfe4e4aa1376d2e1d081650e0f
JunHyung1206 <pjh120610@naver.com>
2025-11-03 03:36:13 +0900

    test(GetResult) : 필요없는 import문 제거

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -1,13 +1,10 @@
 package lotto.domain;
 
 import lotto.exception.LottoValidationException;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
