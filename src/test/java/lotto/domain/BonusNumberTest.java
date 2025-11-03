1051a620df5ae92877d632175523fe631a2a8e78
JunHyung1206 <pjh120610@naver.com>
2025-11-03 13:43:30 +0900

    refactor(InputWinningNumbers) : 보너스 번호 경계값 테스트

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -2,7 +2,6 @@ package lotto.domain;
 
 import lotto.exception.LottoValidationException;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -10,17 +9,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BonusNumberTest {
-    BonusNumber bonusNumber;
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 15, 45})
     @DisplayName("정상적인 동작에 대한 테스트 케이스")
-    void successTestCase() {
-        bonusNumber = new BonusNumber(7);
-        assertThat(bonusNumber.getValue()).isEqualTo(7);
+    void successTestCase(int input) {
+        assertThat(new BonusNumber(input).getValue()).isEqualTo(input);
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {0, 100, -45})
+    @ValueSource(ints = {0, 46, 100, -1})
     @DisplayName("보너스 번호의 입력 값이 1 ~ 45가 아닌 케이스")
     void invalidRangeCase(int input) {
         assertThrows(LottoValidationException.class, () -> new BonusNumber(input));
