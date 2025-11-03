a581cd14c1c2df41f5b8f73432e5a92bc3f07df8
JunHyung1206 <pjh120610@naver.com>
2025-11-03 18:56:01 +0900

    refactor(GetResult) : 생성자 long으로 변경

--- src/main/java/lotto/domain/WinningRank.java
+++ src/main/java/lotto/domain/WinningRank.java
@@ -1,18 +1,18 @@
 package lotto.domain;
 
 public enum WinningRank {
-    FIRST("6개 일치", 2_000_000_000),
-    SECOND("5개 일치, 보너스 볼 일치", 30_000_000),
-    THIRD("5개 일치", 1500_000),
-    FOURTH("4개 일치", 50_000),
-    FIFTH("3개 일치", 5_000),
-    NONE("그외", 0);
+    FIRST("6개 일치", 2_000_000_000L),
+    SECOND("5개 일치, 보너스 볼 일치", 30_000_000L),
+    THIRD("5개 일치", 1_500_000L),
+    FOURTH("4개 일치", 50_000L),
+    FIFTH("3개 일치", 5_000L),
+    NONE("그외", 0L);
 
     private final String label;
     private final long prize;
     private static final WinningRank[] MATCH_COUNT_TABLE = {NONE, NONE, NONE, FIFTH, FOURTH, THIRD, FIRST};
 
-    WinningRank(String description, int prize) {
+    WinningRank(String description, long prize) {
         this.label = description;
         this.prize = prize;
     }
